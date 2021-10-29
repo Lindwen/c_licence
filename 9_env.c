7ef5476b64925efd297282f98bf6073f09413cba
Lindwen <34105327+Lindwen@users.noreply.github.com>
2021-10-29 11:49:09 +0200

    Update 9_env.c

--- 9_env.c
+++ 9_env.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[], char *envp[])
     printf("Appel avec %d paramètres\n", argc);
     printf("Le nom de la commande est %s\n", argv[0]);
     printf("Les arguments de la commandes sont les suivants :");
-    for (k = 1; k < argc; k++) printf("%s ", k, argv[k]);
+    for (k = 1; k < argc; k++) printf("%s ", argv[k]);
     printf("\n Les variables d'environnements sont :\n");
     for (k = 0; envp[k]!=NULL; k++) printf("%s\n", envp[k]);
     
