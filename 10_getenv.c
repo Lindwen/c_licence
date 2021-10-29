fbafc7cf2c9bfe450402b667354f6fff8790202d
Lindwen <34105327+Lindwen@users.noreply.github.com>
2021-10-29 11:53:09 +0200

    Create 10_getenv.c

---
+++ 10_getenv.c
@@ -0,0 +1,16 @@
+/* getenv.c */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    char *terminal; char * login;
+    terminal = getenv("TERM");
+    login=getenv("LOGNAME");
+    printf("Vous êtes connectes sous le nom de %s \n", login);
+    printf("Le terminal est ");
+    if(terminal==NULL)
+    printf("inconnu\n");
+    else
+    printf("un %s\n", terminal);
+    exit(0);
+}
