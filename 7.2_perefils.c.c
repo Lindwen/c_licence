94fe123f3507b45f828e26af36db8277beaff560
Lindwen <34105327+Lindwen@users.noreply.github.com>
2021-10-29 10:23:22 +0200

    Add files via upload

---
+++ 7.2_perefils.c.c
@@ -0,0 +1,22 @@
+/* perefils.c */
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int PID;
+    if ((PID = fork()) == 0)
+    {       /* processus fils */
+        printf("Le fils: mon pid est %d, le pid de mon papa est %d\n", getpid(), getpid());
+        printf("\t\t fork a retourné au fils la valeur %d\n", PID);
+        sleep(5);
+    }
+    else if (PID > 0)
+    {       /* processus père */
+        printf("Le père: mon pid est %d, le pid de mon papa a moi est %d\n", getpid(), getpid());
+        printf("\t\t fork a retourné au père la valeur %d\n", PID);
+        sleep(1);
+    }
+    else("Erreur dans fork !!!");
+    return 0;
+}
