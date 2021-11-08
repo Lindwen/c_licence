b7c79e9bd485cfe3f8bcac7fe1f17f7c6145b4df
Lindwen <34105327+Lindwen@users.noreply.github.com>
2021-11-08 12:07:24 +0100

    Add files via upload

---
+++ 16_masque.c
@@ -0,0 +1,22 @@
+/* masque.c */
+#include <stdio.h>
+#include <signal.h>
+
+struct sigaction action;
+
+void main()
+{
+	action.sa_handler = SIG_IGN;
+	sigaction(SIGINT, &action, NULL);
+	sigaction(SIGQUIT, &action, NULL);
+	printf("Signaux SIGINT et SIGQUIT ignores\n");
+
+	sleep(30);
+
+	printf("\nRetablisseent des signaux SIGINT et SINGQUIT\n");
+	action.sa_handler = SIG_IGN;
+        sigaction(SIGINT, &action, NULL);
+        sigaction(SIGQUIT, &action, NULL);
+
+	while(1);
+}
